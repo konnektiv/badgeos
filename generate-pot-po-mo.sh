0213a0ed8754ce87ea5a09fe447b7902b7c0e63f
Christoph Herbst <chris.p.herbst@gmail.com>
2017-07-07 14:08:40 +0200

    add script to generate translation files

---
+++ generate-pot-po-mo.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+TEXTDOMAIN=${PWD##*/}
+
+#generate POT file from source
+find . -name "*.php" > filelist
+xgettext --keyword=__ --keyword=_e --keyword=_n:1,2 --keyword=_x:1,2c --keyword=_ex:1,2c \
+	--files-from=filelist --language=PHP --package-name BadgeOS -o - | sed 's/charset=CHARSET/charset=UTF-8/' > languages/$TEXTDOMAIN.pot
+rm -f filelist
+
+cd languages
+for filename in ./*.po; do
+    #update PO files from POT
+    msgmerge --update --no-fuzzy-matching --backup=off $filename $TEXTDOMAIN.pot
+
+    #update MO file from PO file
+    msgcat $filename | msgfmt -o $(basename "$filename" .po).mo -
+
+done
+
